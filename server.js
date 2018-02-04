542981e890277908178ac982c4aec0f15fd78e4e
bbalconi <bbalconi@gmail.com>
2018-02-03 20:12:43 -0700

    serverupdate

--- server.js
+++ server.js
@@ -92,6 +92,7 @@ app.post('/deleteCard', (req, res, next) => {
 });
 
 app.get("/*",  (req, res) => {
+  console.log('serving?')
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
